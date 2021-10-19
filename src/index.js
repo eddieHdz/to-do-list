2bd6e2869448bb440ce93ae850f187bd80eb27e7
Eduardo <eduardo.hdzdelarosa@gmail.com>
2021-10-18 19:28:52 -0600

    Primer commit y creacion del proyecto, con los estilos y estructura. Se crearon las clases para controlar las acciones de la lista

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles.css';
+import { Todo, TodoList } from './classes';
+
+const todoList = new TodoList();
+const tarea = new Todo('aprender javascript');
+
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
