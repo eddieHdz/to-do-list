2660d21f4293c5cfc6e6de60922bab51549d01fc
Eduardo <eduardo.hdzdelarosa@gmail.com>
2021-10-19 23:13:54 -0600

    se agrego el codigo html para crear una tarea nueva

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 const todoList = new TodoList();
 const tarea = new Todo('aprender javascript');
@@ -7,3 +8,6 @@ const tarea = new Todo('aprender javascript');
 todoList.nuevoTodo(tarea);
 
 console.log(todoList);
+tarea.completado = false;
+
+crearTodoHtml(tarea);
