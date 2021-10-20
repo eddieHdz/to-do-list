270a5d1224674bfe3198be987a680a6bddfc880b
Eduardo <eduardo.hdzdelarosa@gmail.com>
2021-10-20 14:14:30 -0600

    se creo el metodo para agregar una tarea nueva a la lista

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,9 @@
+import { Todo } from '../classes';
+import { todoList } from '../index';
+
 //Referencias en el HTML
 const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
 export const crearTodoHtml = (todo) => {
     const htmlTodo = `
@@ -18,3 +22,14 @@ export const crearTodoHtml = (todo) => {
 
     return div.firstElementChild;
 }
+
+//eventos
+
+txtInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+    }
+});
