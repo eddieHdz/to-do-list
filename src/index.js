516372dcb28c834b9fa9b9b59ba7fce0d38cffbc
Eduardo <eduardo.hdzdelarosa@gmail.com>
2021-10-20 19:50:36 -0600

    se creo la funcionalidad para guardar las tareas en el localstorage del navegador

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-const tarea = new Todo('aprender javascript');
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+
+
+
+
+
+
+
+
+
+/* const tarea = new Todo('aprender javascript');
 
 todoList.nuevoTodo(tarea);
 tarea.completado = false;
 
 crearTodoHtml(tarea);
+
+localStorage.setItem('mi-key','ABCD');
+setTimeout(() => {
+    localStorage.removeItem('mi-key');
+},1500); */
